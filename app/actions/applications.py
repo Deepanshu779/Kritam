be8726cac725ee6dc60c2bda738cf879c6c420b6
Deepanshu Kumar Pandit <deepanshukumarpandit2024@gmail.com>
2026-09-23 21:15:22 +0530

    Fix reliable Windows app launching

--- app/actions/applications.py
+++ app/actions/applications.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import subprocess
 
 
 class ApplicationManager:
@@ -14,17 +16,43 @@ class ApplicationManager:
             "settings": "ms-settings:",
         }
 
+    def _chrome_candidates(self):
+        local_app_data = os.environ.get("LOCALAPPDATA", "")
+        program_files = os.environ.get("PROGRAMFILES", "")
+        program_files_x86 = os.environ.get("PROGRAMFILES(X86)", "")
+
+        return [
+            shutil.which("chrome.exe"),
+            os.path.join(local_app_data, "Google", "Chrome", "Application", "chrome.exe"),
+            os.path.join(program_files, "Google", "Chrome", "Application", "chrome.exe"),
+            os.path.join(program_files_x86, "Google", "Chrome", "Application", "chrome.exe"),
+        ]
+
     def open_application(self, application_name):
         application_name = application_name.lower().strip()
 
         if application_name not in self.applications:
             return False
 
+        target = self.applications[application_name]
+
         try:
-            os.startfile(self.applications[application_name])
+            if application_name == "chrome":
+                for candidate in self._chrome_candidates():
+                    if candidate and os.path.isfile(candidate):
+                        subprocess.Popen([candidate], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                        return True
+                return False
+
+            if target == "ms-settings:":
+                os.startfile(target)
+                return True
+
+            executable = shutil.which(target) or target
+            subprocess.Popen([executable], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             return True
 
-        except (OSError, ValueError):
+        except (OSError, ValueError, subprocess.SubprocessError):
             return False
 
     def handle_open_application(self, intent):
